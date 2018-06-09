f6b75de7fd8131b722a96fb74b91ad95128329cf
AndrewAlmighty <xaidianx@gmail.com>
2018-06-09 23:26:01 +0200

    Saving!

--- ESI/ESI.py
+++ ESI/ESI.py
@@ -12,71 +12,6 @@ class App:
 		self.builder.add_from_file("Gui.glade")
 		self.builder.connect_signals(SignalHandler())
 		
-class FileChooserWindow(Gtk.Window):
-
-    def __init__(self):
-        Gtk.Window.__init__(self, title="FileChooser")
-
-        box = Gtk.Box(spacing=6)
-        self.add(box)
-
-        button1 = Gtk.Button("Choose File")
-        button1.connect("clicked", self.on_file_clicked)
-        box.add(button1)
-
-        button2 = Gtk.Button("Choose Folder")
-        button2.connect("clicked", self.on_folder_clicked)
-        box.add(button2)
-
-    def on_file_clicked(self, widget):
-        dialog = Gtk.FileChooserDialog("Please choose a file", self,
-            Gtk.FileChooserAction.OPEN,
-            (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
-             Gtk.STOCK_OPEN, Gtk.ResponseType.OK))
-
-        self.add_filters(dialog)
-
-        response = dialog.run()
-        if response == Gtk.ResponseType.OK:
-            print("Open clicked")
-            print("File selected: " + dialog.get_filename())
-        elif response == Gtk.ResponseType.CANCEL:
-            print("Cancel clicked")
-
-        dialog.destroy()
-
-    def add_filters(self, dialog):
-        filter_text = Gtk.FileFilter()
-        filter_text.set_name("Text files")
-        filter_text.add_mime_type("text/plain")
-        dialog.add_filter(filter_text)
-
-        filter_py = Gtk.FileFilter()
-        filter_py.set_name("Python files")
-        filter_py.add_mime_type("text/x-python")
-        dialog.add_filter(filter_py)
-
-        filter_any = Gtk.FileFilter()
-        filter_any.set_name("Any files")
-        filter_any.add_pattern("*")
-        dialog.add_filter(filter_any)
-
-    def on_folder_clicked(self, widget):
-        dialog = Gtk.FileChooserDialog("Please choose a folder", self,
-            Gtk.FileChooserAction.SELECT_FOLDER,
-            (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,
-             "Select", Gtk.ResponseType.OK))
-        dialog.set_default_size(800, 400)
-
-        response = dialog.run()
-        if response == Gtk.ResponseType.OK:
-            print("Select clicked")
-            print("Folder selected: " + dialog.get_filename())
-        elif response == Gtk.ResponseType.CANCEL:
-            print("Cancel clicked")
-
-        dialog.destroy()	
-		
 class SignalHandler:
 	def __init__(self):
 		self.option_A = False
@@ -95,6 +30,7 @@ class SignalHandler:
 		
 	def on_MenuSave_activate(self, *args):
 		x = FileChooserWindow()
+		x.on
 		
 	def on_MenuOpen_activate(self, *args):
 		x = FileChooserWindow()
@@ -116,7 +52,7 @@ class SignalHandler:
 
 	def on_ExecuteButton_clicked(self, ExecuteButton):
 		psArgs = app.builder.get_object("PSArgs").get_text()
-		print(psArgs)
+
 		if not psArgs:
 
 			if self.option_A:
@@ -151,9 +87,12 @@ class SignalHandler:
 			proc = subprocess.Popen(['ps', psArgs], stdout = subprocess.PIPE)
 
 		out, err = proc.communicate()
-		app.builder.get_object("OutputText").get_buffer().set_text(out.decode("utf-8"))
-		app.builder.get_object("OutputText").get_buffer().get_end_iter()
-			
+		
+		if not out:
+			print ("BLAD!")
+				
+		else:
+			app.builder.get_object("OutputText").get_buffer().set_text(out.decode("utf-8"))			
 
 	def on_Basic_clicked(self, Basic):
 		app.builder.get_object("OptionsCheckBoxes").set_opacity(1.0)
