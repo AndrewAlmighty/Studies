9099aed24fee46c659d9a9bd9541725194671aa9
AndrewAlmighty <xaidianx@gmail.com>
2018-08-15 13:00:16 +0200

    Fixed port problem

--- Berkeley_algorithm/networkmethods.c
+++ Berkeley_algorithm/networkmethods.c
@@ -24,7 +24,7 @@ enum serverStatus createUDPServer(int *server_socket, const int *port)
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons(port);
+    server_addr.sin_port = htons(*port);
 
     if(bind(*server_socket, (const struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
     {
