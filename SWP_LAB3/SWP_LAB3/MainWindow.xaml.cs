8ca65962d42ed2b32393cfcb44144f7674bfafb1
Andrzej Such <andrzej.such@student.wat.edu.pl>
2019-01-22 14:30:24 +0100

    New fast app for SWP

---
+++ SWP_LAB3/SWP_LAB3/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SWP_LAB3
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private StackPanel m_stackPanel;
+        private event MouseButtonEventHandler m_click;
+        private BitmapImage[] m_bitMaps = new BitmapImage[5];
+        private Image[] m_images = new Image[5];
+        private String[] m_imageNames = { "czekolada.jpg", "banany.jpg", "belwedere.jpg", "lays.jpg", "tatra.jpg" };
+        public MainWindow()
+        {
+            InitializeComponent();
+            m_stackPanel = new StackPanel();
+            m_stackPanel.MouseLeftButtonUp += new MouseButtonEventHandler(StackPanel_OnMouseUp);
+            m_stackPanel.Orientation = Orientation.Horizontal;
+            m_stackPanel.HorizontalAlignment = HorizontalAlignment.Left;
+            m_stackPanel.VerticalAlignment = VerticalAlignment.Top;
+            for (int i = 0; i < 5; i++)
+            {
+                m_bitMaps[i] = new BitmapImage(new Uri(".\\Images\\" + m_imageNames[i], UriKind.Relative));
+                m_images[i] = new Image();
+                m_images[i].Name = "obrazek_" + i.ToString();
+                m_images[i].Source = m_bitMaps[i];
+                m_images[i].Width = 100;
+                m_images[i].Height = 100;
+                m_stackPanel.Children.Add(m_images[i]);
+            }
+
+            MainScrollViewer.Content = m_stackPanel;
+        }
+
+        private void StackPanel_OnMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Released)
+            {
+                Image tmp = (Image)e.OriginalSource;
+                InfoTextBox.Text = tmp.Name;
+            }
+        }
+    }
+}
---
+++ SWP_LAB3/SWP_LAB3/MyItem.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace SWP_LAB3
+{
+    class MyItem
+    {
+        private String m_name;
+        private String m_description;
+        private String m_price;
+        private BitmapImage m_bitmap;
+        private Image m_image;
+        
+        public MyItem(String name, String description, String price, String path_to_image)
+        {
+            m_name = name;
+            m_description = description;
+            m_price = price;
+            m_bitmap = new BitmapImage(new Uri(path_to_image, UriKind.Relative));
+
+        }
+
+        public String getName()
+        {
+            return m_name;
+        }
+
+        public String getDescription()
+        {
+            return m_description;
+        }
+
+        public String getPrice()
+        {
+            return m_price;
+        }
+    }
+}
